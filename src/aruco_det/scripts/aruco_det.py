234826b224ecb04d7cec4a3f5902e08fe8faa3bf
zypp1 <1406790796@qq.com>
2024-05-27 21:40:10 -0700

    simulation

---
+++ src/aruco_det/scripts/aruco_det.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from geometry_msgs.msg import PoseStamped, TwistStamped, Point
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool,Int32
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+from math import atan
+
+class Algorithm:
+    
+    def __init__(self):
+        rospy.init_node("arcuo_det")
+        self.debug = rospy.get_param('~is_debug')
+        self.state = Odometry()
+
+        # for Aruco
+        self.bridge = CvBridge()
+        self.arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
+        self.arucoParams = cv2.aruco.DetectorParameters_create()
+        
+        self.camera_Matrix = np.array([[1.78634438e+03,0.00000000e+00,9.13022592e+02],
+                                        [0.00000000e+00,1.82753404e+03,4.87987317e+02],
+                                        [0.00000000e+00,0.00000000e+00,1.00000000e+00]])
+
+        self.distortion_Matrix = np.array([4.51202586e-01,-2.62210378e+00,-5.92390289e-02,2.86948250e-03,4.59687027e+00])
+
+        self.q_camera = np.array([0.707, 0, 0, -0.707])
+
+        # Subscriber
+        pose_sub = rospy.Subscriber('/Odometry', Odometry, self.pose_callback)
+        usb_cam_sub = rospy.Subscriber('/rflysim/sensor2/img_rgb', Image, self.camera_callback)
+
+        # Publisher 
+        self.id_pub = rospy.Publisher('/aruco_det/aruco_id', Int32, queue_size=1)
+        self.camera_pub = rospy.Publisher('/aruco_det/vis', Image, queue_size=1)
+        self.det_vis_pub = rospy.Publisher('/aruco_det/det_vis', Image, queue_size=1)
+        self.detect_result = rospy.Publisher('/aruco_det/target_loc', PoseStamped, queue_size=1)
+        self.detect_result_local = rospy.Publisher('/aruco_det/target_loc_local', PoseStamped, queue_size=1)
+
+    def pose_callback(self, msg):
+        self.state = msg
+    
+    def quat_mul(self, q1:np.array, q2:np.array):
+        w1, x1, y1, z1 = q1
+        w2, x2, y2, z2 = q2
+        q = np.array([
+            w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
+            w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
+            w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
+            w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+        ])
+        return q
+
+    def quat_rot_vector(self, q:np.array, v:np.array):
+        q = q / np.linalg.norm(q)
+        q_inv = np.array([q[0], -q[1], -q[2], -q[3]])
+        qv = self.quat_mul(self.quat_mul(q, np.concatenate([[0], v])), q_inv)
+        return qv[1 : ]
+    
+    def camera_callback(self, msg):
+        msg = self.bridge.imgmsg_to_cv2(msg)
+        self.camera_pub.publish(self.bridge.cv2_to_imgmsg(msg))
+        time_received = rospy.Time.now()
+        now_state = self.state
+        cv_image = msg
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(cv_image, self.arucoDict,
+            parameters=self.arucoParams)
+        
+        if len(corners) > 0:
+            detected_id = ids[0][0]
+            self.id_pub.publish(detected_id)
+            rospy.loginfo(f"Detected Aruco ID: {detected_id}")
+            if ids[0][0] == 19:
+                length = 0.2
+            else:
+                length = 0.025
+                
+            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, length, self.camera_Matrix, self.distortion_Matrix)
+            for i in range(rvec.shape[0]):
+                
+                target_loc = PoseStamped()
+                target_loc.header.stamp = time_received
+
+                pos_camera = np.array([tvec[0][0][0],
+                                       tvec[0][0][1],
+                                       -tvec[0][0][2]])
+                target_loc_local = PoseStamped()
+                target_loc_local.pose.position.x = corners[0].sum(axis=1)[0][0] / 4
+                target_loc_local.pose.position.y = corners[0].sum(axis=1)[0][1] / 4
+                self.detect_result_local.publish(target_loc_local)
+                pos_in_drone = self.quat_rot_vector(self.q_camera, pos_camera)
+                q_drone = np.array([now_state.pose.pose.orientation.w,
+                                    now_state.pose.pose.orientation.x,
+                                    now_state.pose.pose.orientation.y,
+                                    now_state.pose.pose.orientation.z])
+                target_loc.pose.position.x, target_loc.pose.position.y, target_loc.pose.position.z = \
+                    self.quat_rot_vector(q_drone, pos_in_drone)
+                target_loc.pose.position.x += now_state.pose.pose.position.x
+                target_loc.pose.position.y += now_state.pose.pose.position.y
+                target_loc.pose.position.z += now_state.pose.pose.position.z
+
+                self.detect_result.publish(target_loc)
+                if self.debug:
+                    draw_det_marker_img = cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
+                    # draw_det_marker_img = cv2.drawFrameAxes(draw_det_marker_img, self.camera_Matrix, self.distortion_Matrix,
+                    #                                             rvec[i, :, :], tvec[i, :, :], 0.03)
+                    self.det_vis_pub.publish(self.bridge.cv2_to_imgmsg(draw_det_marker_img))
+                    print("detect time: ", rospy.Time.now().to_sec())
+                break
+
+if __name__ == "__main__":
+
+    main_algorithm = Algorithm()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        rate.sleep()
---
+++ src/control/uav_control/script/setpoint.py
@@ -0,0 +1,151 @@
+#! /usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.srv import SetMode, CommandBool
+from std_msgs.msg import Bool,Int32
+class Controller:
+    def __init__(self) -> None:
+        rospy.init_node("setpoint")
+        self.load_params()
+        self.pub_point = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=1)
+        self.set_mode_client = rospy.ServiceProxy("/mavros/set_mode", SetMode)
+        self.arm_client = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+        self.sleep_time = 0.2
+        self.sleep_time_time = 1.0
+        self.center_pos =[0,0,0]
+        self.center_pos_start =[0,0,0]
+        self.center_pos_end =[0,0,0]
+        self.det_flag = False
+        rospy.Subscriber("~odom", Odometry, self.odom_cb, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/aruco_det/aruco_id', Int32, self.detected_callback)
+        rospy.Subscriber("/center_position", PoseStamped, self.pose_callback)
+
+    def pose_callback(self, msg):
+        self.center_pos = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+
+    def get_center_pos(self):
+        self.center_pos_start = [self.center_pos[0] -0.1, self.center_pos[1], self.center_pos[2]]
+        self.center_pos_end = [self.center_pos[0] + 0.1, self.center_pos[1], self.center_pos[2]]
+        print(f"Calculated center_pos_start: {self.center_pos_start}")
+        print(f"Calculated center_pos_end: {self.center_pos_end}")
+        
+    def load_params(self):
+        self.state = 0
+        self.takeoff_point = rospy.get_param('~takeoff')
+        self.first_square_point_pre = rospy.get_param('~1st_square_pre')
+        self.first_square_point = rospy.get_param('~1st_square')
+        self.first_square_point_end = rospy.get_param('~1st_square_end')
+        self.second_square_point_pre = rospy.get_param('~2nd_square_pre')
+        self.second_square_point_end = rospy.get_param('~2nd_square_end')
+        self.car_up_point = rospy.get_param('~car_up')
+        self.car_point = rospy.get_param('~car')
+        self.land_point = rospy.get_param('~land')
+        self.land_point_end = rospy.get_param('~land_end')
+        print(type(self.land_point))
+
+    def pub(self, point: list):
+        target_pose = PoseStamped()
+        target_pose.pose.position.x = point[0]
+        target_pose.pose.position.y = point[1]
+        target_pose.pose.position.z = point[2]
+        target_pose.pose.orientation.w = 1
+        target_pose.pose.orientation.x = 0
+        target_pose.pose.orientation.y = 0
+        target_pose.pose.orientation.z = 0
+        self.pub_point.publish(target_pose)
+
+    def is_close(self, odom: Odometry, point: list):
+        if (odom.pose.pose.position.x - point[0])**2 + \
+           (odom.pose.pose.position.y - point[1])**2 + \
+           (odom.pose.pose.position.z - point[2])**2 < 0.2:
+            return True
+        return False 
+
+    def detected_callback(self, msg):
+        self.det_flag = True
+
+    def odom_cb(self, msg: Odometry):
+        print(self.state)
+        if self.state == 0:
+            if self.is_close(msg, self.takeoff_point):
+                rospy.sleep(self.sleep_time)
+                self.state = 1
+                pass
+            else:
+                self.pub(self.takeoff_point)
+        if self.state == 1:
+            if self.is_close(msg, self.first_square_point_pre):
+                rospy.sleep(self.sleep_time_time)
+                self.state = 2
+                self.get_center_pos()
+            else:
+                self.pub(self.first_square_point_pre)
+        if self.state == 2:
+            if self.is_close(msg, self.center_pos_start):
+                rospy.sleep(self.sleep_time)
+                self.state = 3
+            else:
+                self.pub(self.center_pos_start)
+        if self.state == 3:
+            if self.is_close(msg, self.first_square_point_end):
+                rospy.sleep(self.sleep_time_time)
+                self.state = 4
+                self.get_center_pos()
+            else:
+                self.pub(self.first_square_point_end)
+        if self.state == 4:
+            if self.is_close(msg, self.center_pos_start):
+                rospy.sleep(self.sleep_time)
+                self.state = 5
+            else:
+                self.pub(self.center_pos_start)
+        if self.state == 5:
+            if self.is_close(msg, self.second_square_point_end):
+                rospy.sleep(self.sleep_time)
+                self.state = 6
+                pass
+            else:
+                self.pub(self.second_square_point_end)
+        if self.state == 6:
+            if self.is_close(msg, self.car_up_point):
+                rospy.sleep(self.sleep_time)
+                self.state = 7
+                pass
+            else:
+                self.pub(self.car_up_point)
+        if self.state == 7:
+            if self.det_flag:
+                rospy.sleep(self.sleep_time)
+                self.state = 8
+                pass
+            else:
+                self.pub(self.car_point)
+        if self.state == 8:
+            if self.is_close(msg, self.land_point):
+                rospy.sleep(self.sleep_time)
+                self.state = 9
+                pass
+            else:
+                self.pub(self.land_point)
+        if self.state == 9:
+            if self.is_close(msg, self.land_point_end):
+                rospy.sleep(self.sleep_time)
+                self.state = 10
+                pass
+            else:
+                self.pub(self.land_point_end)
+        if self.state == 10:
+            # self.set_mode_client(0,'AUTO.LAND')
+            # rospy.sleep(self.sleep_time)
+            # self.arm_client(False)
+            pass
+            
+        
+
+
+controller = Controller()
+
+rospy.spin()
